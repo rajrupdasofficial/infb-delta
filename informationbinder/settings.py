a8ad69d2b69871b6a56903cc08f506d58154128a
RajrupDasid <rajrupdasid@outlook.com>
2021-11-03 13:23:55 +0530

    ckeitor ajax cdn upgraded

--- informationbinder/settings.py
+++ informationbinder/settings.py
@@ -141,7 +141,7 @@ STATICFILES_STORAGE='whitenoise.storage.CompressManifestStaticFilesStorage'
 # https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
-CKEDITOR_JQUERY_URL = 'https://ajax.googleapis.com/ajax/libs/jquery/2.2.4/jquery.min.js'
+CKEDITOR_JQUERY_URL = 'https://ajax.googleapis.com/ajax/libs/jquery/3.6.0/jquery.min.js'
 CKEDITOR_UPLOAD_PATH = "uploads/"
 CKEDITOR_IMAGE_BACKEND ="pillow"
 CKEDITOR_CONFIGS = {
