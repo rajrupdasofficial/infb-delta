0da2ae8c5cc1c46c80dd2cd845e97b3a4f7c2014
RajrupDasid <rajrupdasid@outlook.com>
2021-11-03 17:38:50 +0530

    fixing ckeditor problems

--- informationbinder/urls.py
+++ informationbinder/urls.py
@@ -1,13 +1,14 @@
 from django.contrib import admin
 from django.urls import path
 from django.conf.urls.static import static
-from django.conf.urls import include
+from django.conf.urls import include,url
 from django.conf import settings
 
 urlpatterns = [
+    url(r'^ckeditor/', include('ckeditor_uploader.urls')),
     path('admin/', admin.site.urls),
     path('app/',include('django.contrib.auth.urls')),
     path('',include('app.urls')),
-    path('ckeditor/',include('ckeditor_uploader.urls')),
+    #path('ckeditor/',include('ckeditor_uploader.urls')),
 ]+ static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT) 
 urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
